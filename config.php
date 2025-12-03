4a76cc644ce2870211d8e44215d1e850d5f91496
lyure299 <lucasyure71@gmail.com>
2025-12-03 11:19:14 -0300

    formulario php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php 
+
+$dbHost = 'Localhost';
+$dbUsername = 'root';
+$dbPassoword = 'Lucasyure20@';
+$dbName = 'formulario';
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassoword,$dbName);
+
+//if($conexao->connect_errno){
+//    echo "Erro";
+//}
+//else{
+//  echo "conexâo efetuado com sucesso";
+//}
+?>
